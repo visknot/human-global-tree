e1bc8584e08f1300a5352a7f3b3c7904b106d943
xtinaushakova <kristina.ushakova@outlook.com>
2020-05-20 21:12:35 +0300

    Script now flips ND6

--- Head/2Scripts/FulltreeAddData2Table.py
+++ Head/2Scripts/FulltreeAddData2Table.py
@@ -2,12 +2,12 @@
 
 REQUIREMENTS: This script requires biopython to run.
 
-This simple script appends data on aminoacid substitutions to fulltree.csv table. 
+This simple script appends data on aminoacid substitutions to fulltree.csv table.
 It determines ancestral/derived aa in accordance to mitochondrial translation table.
-Adds the following columns: 
+Adds the following columns:
 	"pos_in_codon" - position of substitution in codon (1, 2, 3)
-	"synonymous" - is the mutation synonymous or not 
-	"ancestral_aa", "derived_aa" - self explanatory 
+	"synonymous" - is the mutation synonymous or not
+	"ancestral_aa", "derived_aa" - self explanatory
 	"note" - any pecularities that should be noted
 
 If the substitution ocurred in a non-coding sequence the script appends NAs to all columns
@@ -19,13 +19,14 @@ import os
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
+from Bio.Seq import reverse_complement
 
 # Path name for opening files
 dirname = os.path.dirname(os.path.abspath(__file__))
 
 # Parse Genbank, extract gene start
 refGenes = {}
-refGenbank = os.path.join(dirname, "../../Body/3Results/NC_012920.1.gb")
+refGenbank = os.path.join(dirname, "/Users/xtinaushakova/mitoclub/human-global-tree/Body/1Raw/NC_012920.1.gb")
 refGenbank = open(refGenbank)
 
 # Parse record
@@ -37,13 +38,13 @@ for rec in SeqIO.parse(refGenbank, "genbank"):
 refGenbank.close()
 
 # Open our table. This is the way around relative paths in Python
-sourceTablePath = os.path.join(dirname, "../../Body/3Results/fulltree.csv")
+sourceTablePath = os.path.join(dirname, "/Users/xtinaushakova/mitoclub/human-global-tree/Body/1Raw/fulltree.csv")
 sourceTable = open(sourceTablePath)
 
 # Aminoacid reference
 aminoacids = {'C' : 'Cys', 'D' : 'Asp', 'S' : 'Ser', 'Q' : 'Gln', 'K' : 'Lys',
-     		  'I' : 'Ile', 'P' : 'Pro', 'T' : 'Thr', 'F' : 'Phe', 'N' : 'Asn', 
-              'G' : 'Gly', 'H' : 'His', 'L' : 'Leu', 'R' : 'Arg', 'W' : 'Trp', 
+     		  'I' : 'Ile', 'P' : 'Pro', 'T' : 'Thr', 'F' : 'Phe', 'N' : 'Asn',
+              'G' : 'Gly', 'H' : 'His', 'L' : 'Leu', 'R' : 'Arg', 'W' : 'Trp',
               'A' : 'Ala', 'V' : 'Val', 'E' : 'Glu', 'Y' : 'Tyr', 'M' : 'Met',
               '*' : 'Stop', 'X' : 'Ambiguous', 'J' : 'Leu/Ile', 'B' : 'Asn/Asp',
               'Z' : 'Gln/Glu'}
@@ -51,12 +52,12 @@ aminoacids = {'C' : 'Cys', 'D' : 'Asp', 'S' : 'Ser', 'Q' : 'Gln', 'K' : 'Lys',
 position = {1 : 1, 2 : 2, 0 : 3}
 
 # Open new table fulltreeCodons for writing
-newTablePath = os.path.join(dirname, "../../Body/3Results/fulltreeCodons.csv")
+newTablePath = os.path.join(dirname, "/Users/xtinaushakova/mitoclub/human-global-tree/Body/2Derived/fulltreeCodons.csv")
 newTable = open(newTablePath, 'wt')
 
 # Read header in old file append and store in new
 header = sourceTable.readline().strip('\n')
-newHeader = ";".join([header, "gene_start", "pos_in_codon", "synonymous", "ancestral_aa", "derived_aa", "note"]) 
+newHeader = ";".join([header, "gene_start", "pos_in_codon", "synonymous", "ancestral_aa", "derived_aa", "note", "ancestral_codon", "derived_codon"])
 newTable.write(newHeader + "\n")
 
 for line in sourceTable:
@@ -82,30 +83,41 @@ for line in sourceTable:
 		pos = int(line[3])
 
 		# beginning of codon, beginning of gene from ref
-		posOfNucl = pos - geneStart + 2
+
+		# frame shift by gene
+		frameShift = 1
+
+		posOfNucl = pos - geneStart + frameShift
 		posInCodon = position[posOfNucl % 3]
 		codonStart = 3 - posInCodon
 		codonEnd = codonStart + 3
 		ancestralCodon = ancestralSeq[codonStart:codonEnd]
 		derivedCodon = derivedSeq[codonStart:codonEnd]
-
 		# If gaps in either sequences then ignore
 		if '-' in ancestralCodon or '-' in derivedCodon:
-			line.extend([geneStart, posInCodon, "NA", "NA", "NA", "gaps"])
+			line.extend([geneStart, posInCodon, "NA", "NA", "NA", "gaps", "NA", "NA"])
 			next
 		else:
-			ancestralCodon = Seq(ancestralCodon, generic_dna)
-			derivedCodon = Seq(derivedCodon, generic_dna)
-			ancestralAa = aminoacids[ancestralCodon.translate(table=2)]
-			derivedAa = aminoacids[derivedCodon.translate(table=2)]
+			ancestralCodonSeq = Seq(ancestralCodon, generic_dna)
+
+			derivedCodonSeq = Seq(derivedCodon, generic_dna)
+			if nameShort == "ND6":
+				print("ND6")
+				print(ancestralCodon)
+				ancestralCodonSeq = Seq(reverse_complement(ancestralCodon), generic_dna)
+
+				print(ancestralCodonSeq)
+				derivedCodonSeq = Seq(reverse_complement(derivedCodon), generic_dna)
+			ancestralAa = aminoacids[ancestralCodonSeq.translate(table=2)]
+			derivedAa = aminoacids[derivedCodonSeq.translate(table=2)]
 			synonymous = "non-synonymous"
 			#synonymous or not
 			if ancestralAa == derivedAa:
 				synonymous = "synonymous"
 			note = "normal"
-			line.extend([geneStart, posInCodon, synonymous, ancestralAa, derivedAa, note])
+			line.extend([geneStart, posInCodon, synonymous, ancestralAa, derivedAa, note, ancestralCodon, derivedCodon])
 	else:
-		line.extend(["NA", "NA", "NA", "NA", "NA", "non-coding"])
+		line.extend(["NA", "NA", "NA", "NA", "NA", "non-coding", "NA", "NA"])
 	line = ";".join(str(element) for element in line)
 	newTable.write(line + "\n")
 
