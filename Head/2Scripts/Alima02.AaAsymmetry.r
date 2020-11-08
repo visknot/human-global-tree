143c61df437074225c1b28b9d38cbe441a7be0a6
konstantinpopadin <konstantinpopadin@gmail.com>
2020-11-08 01:11:17 +0100

    asymmetry works!

--- Head/2Scripts/Alima02.AaAsymmetry.r
+++ Head/2Scripts/Alima02.AaAsymmetry.r
@@ -1,40 +1,67 @@
 rm(list=ls(all=TRUE)) 
 
 #indata <- read.csv("/home/alima/arrr/fulltreeCodons.csv", header = TRUE, sep = ";") #читаю файл 
-indata = read.csv("../../Body/2Derived/fulltreeCodons.csv", header = TRUE, sep = ";")
+data = read.csv("../../Body/2Derived/fulltreeCodons.csv", header = TRUE, sep = ";")
 
 #### FILTER OUT: 
-data <- subset(indata, synonymous == "non-synonymous" & derived_aa != "Ambiguous"& gene_info != "mRNA_ND6")  #убираю лишнее
+data <- subset(data, synonymous == "non-synonymous" & derived_aa != "Ambiguous" & derived_aa != "Asn/Asp" & derived_aa != "Gln/Glu" & derived_aa != "Leu/Ile" & gene_info != "mRNA_ND6")  #убираю лишнее
+table(data$derived_aa)
 
 ## ADD FILTER OF BACKGROUND (THE SAME NEIGHBOR NUCLEOTIDES)
 
 
-## DERIVE SUBSTITUTION MATRIX
-
+##### DERIVE SUBSTITUTION MATRIX FromTo
 data$FromTo = paste(data$ancestral_aa,data$derived_aa,sep = '>')
-
 FromTo = data.frame(table(data$FromTo))
 names(FromTo) = c('FromAncestralToDerived', 'NumberOfEvents')
 nrow(FromTo) # 236, but totally there are 21*21 possibilities
-AllAa = data.frame(unique(data$ancestral_aa)); nrow(AllAa) # including stop
+FromTo$FromAncestralToDerived = as.character(FromTo$FromAncestralToDerived)
 
-# Make it into both directions: A1 A2 
+##### ADD DUMMY MATRIX WITH ZEROES 
+AllAa1 = data.frame(unique(data$ancestral_aa)); nrow(AllAa1); names(AllAa1) = c('AA1')
+AllAa2 = data.frame(unique(data$ancestral_aa)); nrow(AllAa2); names(AllAa2) = c('AA2')
+DummyZeroes = merge(AllAa1,AllAa2)
+DummyZeroes$FromAncestralToDerived=paste(DummyZeroes$AA1,DummyZeroes$AA2,sep='>')
+DummyZeroes$NumberOfEvents = 0
+DummyZeroes = DummyZeroes[c(3,4)]
 
-#### we expect majority of ratios > 0
-## 1st nucleotide, T>C
-FromTo[FromTo$FromAncestralToDerived == 'Phe>Leu',]$NumberOfEvents / FromTo[FromTo$FromAncestralToDerived == 'Leu>Phe',]$NumberOfEvents
-FromTo[FromTo$FromAncestralToDerived == 'Ser>Pro',]$NumberOfEvents / FromTo[FromTo$FromAncestralToDerived == 'Pro>Ser',]$NumberOfEvents
-FromTo[FromTo$FromAncestralToDerived == 'Tyr>His',]$NumberOfEvents / FromTo[FromTo$FromAncestralToDerived == 'His>Tyr',]$NumberOfEvents
-FromTo[FromTo$FromAncestralToDerived == 'Cys>Arg',]$NumberOfEvents / FromTo[FromTo$FromAncestralToDerived == 'Arg>Cys',]$NumberOfEvents
-FromTo[FromTo$FromAncestralToDerived == 'Trp>Arg',]$NumberOfEvents / FromTo[FromTo$FromAncestralToDerived == 'Arg>Trp',]$NumberOfEvents
-FromTo[FromTo$FromAncestralToDerived == 'Trp>Arg',]$NumberOfEvents / FromTo[FromTo$FromAncestralToDerived == 'Arg>Trp',]$NumberOfEvents
-FromTo[FromTo$FromAncestralToDerived == 'Stop>Gln',]$NumberOfEvents / FromTo[FromTo$FromAncestralToDerived == 'Gln>Stop',]$NumberOfEvents 
+##### rbind and aggregate FromTo and DummyZeroes
+FromTo1 = rbind(FromTo,DummyZeroes)
+FromTo2 = aggregate(FromTo1$NumberOfEvents, by = list(FromTo1$FromAncestralToDerived), FUN = sum)
+names(FromTo2)=c('FromAncestralToDerived','NumberOfEvents')
+FromTo2$From = gsub(">.*",'',FromTo2$FromAncestralToDerived)
+FromTo2$To = gsub(".*>",'',FromTo2$FromAncestralToDerived)
+FromTo2 = FromTo2[FromTo2$From != FromTo2$To,]; nrow(FromTo2)
 
-## 2nd nucleotide, T>C = we expect ratios > 0:
+##### DERIVE UNIQUE IDENTIFIER FOR EACH PAIR OF AMINOACIDS:
+for (i in 1:nrow(FromTo2)) {FromTo2$AaPairId[i] = paste(sort(unlist(strsplit(FromTo2$FromAncestralToDerived[i],'>'))),collapse = '>') }
 
-#### make this dataset automatically:
-FromTo$From = gsub(">.*",'',FromTo$FromAncestralToDerived)
-FromTo$To = gsub(".*>",'',FromTo$FromAncestralToDerived)
+##### from 420 to 210 rows and NumberOfEvents1To2
+A = FromTo2[FromTo2$FromAncestralToDerived == FromTo2$AaPairId,]; A= A[c(1,2,5)]; names(A)=c('FromAncestralToDerived1','NumberOfEvents1','AaPairId')
+B = FromTo2[FromTo2$FromAncestralToDerived != FromTo2$AaPairId,]; B= B[c(1,2,5)]; names(B)=c('FromAncestralToDerived2','NumberOfEvents2','AaPairId')
+FromTo3 = merge(A,B)
+FromTo3$NumberOfEvents1To2 = FromTo3$NumberOfEvents1 / FromTo3$NumberOfEvents2 
 
-# Merge with Alima file and obtain column: expected: 1 or 0
+##### read expectations:
+exp = read.table("../../Body/2Derived/ExpectedAaSubstitutionDirection.txt", header = TRUE, sep = " ")
+for (i in 1:nrow(exp)) {exp$AaPairId[i] = paste(sort(unlist(strsplit(exp$ExpectedAminoAcidSubstBias[i],'>'))),collapse = '>') }
+exp=exp[c(3,4)]
+FromTo4 = merge(FromTo3,exp, by = 'AaPairId', all.x = TRUE)
+
+##### 
+FromTo4$ExpectedMoreThanOne=NA
+for (i in 1:nrow(FromTo4))
+{ # i = 17
+  if (!is.na(FromTo4$ExpectedAminoAcidSubstBias[i]) & FromTo4$ExpectedAminoAcidSubstBias[i] == FromTo4$FromAncestralToDerived1[i]) {FromTo4$ExpectedMoreThanOne[i] = FromTo4$NumberOfEvents1[i] / FromTo4$NumberOfEvents2[i]}
+  if (!is.na(FromTo4$ExpectedAminoAcidSubstBias[i]) & FromTo4$ExpectedAminoAcidSubstBias[i] == FromTo4$FromAncestralToDerived2[i]) {FromTo4$ExpectedMoreThanOne[i] = FromTo4$NumberOfEvents2[i] / FromTo4$NumberOfEvents1[i]}
+}
+
+FromTo4 = FromTo4[order(FromTo4$ExpectedMoreThanOne),]
+
+
+summary(FromTo4$ExpectedMoreThanOne)
+hist(FromTo4$ExpectedMoreThanOne,breaks = 100)
+wilcox.test(FromTo4$ExpectedMoreThanOne, mu = 1)
+
+###### controls and permutations  (how to kill signal and derive real p-value)?
 
