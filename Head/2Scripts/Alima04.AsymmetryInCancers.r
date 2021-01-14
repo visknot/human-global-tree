8dbffd1a4547e34ce452bd2d4ce73b8b75e932a3
konstantinpopadin <konstantinpopadin@gmail.com>
2021-01-14 23:23:35 +0100

    cancers work perfectly

--- Head/2Scripts/Alima04.AsymmetryInCancers.r
+++ Head/2Scripts/Alima04.AsymmetryInCancers.r
@@ -1,35 +1,119 @@
 rm(list=ls(all=TRUE)) 
 
-pdf("../../Body/4Figures/Alima04.AsymmetryInVertebratePolymorphisms.r.pdf")
+pdf("../../Body/4Figures/Alima04.AsymmetryInCancers.r.pdf")
 
-data = read.table("../../Body/1Raw/VertebratePolymorphisms.MutSpecData.OnlyFourFoldDegCytb.txt", header = TRUE)
+data = read.table("../../Body/1Raw/CancerDataFromCampbell/mtDNA_snv_Oct2016.txt", header = TRUE, sep = '\t')
 
-FourNonZeroTransitions = data[data$T_C > 0 & data$C_T > 0 & data$A_G > 0 & data$G_A > 0,]
-FourNonZeroTransitions$Ah_GhDivideByTh_Ch = FourNonZeroTransitions$T_C / FourNonZeroTransitions$A_G
-FourNonZeroTransitions$Ch_ThDivideByGh_Ah = FourNonZeroTransitions$G_A / FourNonZeroTransitions$C_T
-summary(FourNonZeroTransitions$Ah_GhDivideByTh_Ch)
-summary(FourNonZeroTransitions$Ch_ThDivideByGh_Ah)
-boxplot(FourNonZeroTransitions$Ah_GhDivideByTh_Ch,FourNonZeroTransitions$Ch_ThDivideByGh_Ah, names = c('Ah_GhDivideByTh_Ch','Ch_ThDivideByGh_Ah'), notch = TRUE, outline = FALSE, main = 'all chordata', ylab = 'asymmetry')
+data=data[grepl('nsSNP', data$Annot),]
+for (i in 1:nrow(data)) {data$AaChanges[i] = unlist(strsplit(data$Annot[i],','))[5]}
+data$ancestral_aa = gsub("\\d(.*)",'',data$AaChanges)
+data$derived_aa = gsub("(.*)\\d",'',data$AaChanges)
+table(data$ancestral_aa)
+table(data$derived_aa)
 
-GL = read.table("../../Body/1Raw/GenerationLenghtforMammals.xlsx.txt", header = TRUE, sep = '\t')
-GL$Species = gsub(' ','_',GL$Scientific_name)
+# from one letter to three letter code AA:
+A = c("G","A","L","M","F","W","K","Q","E","S","P","V","I","C","Y","H","R","N","D","T","*")
+AAA = c("Gly","Ala","Leu","Met","Phe","Trp","Lys","Gln","Glu","Ser","Pro","Val","Ile","Cys","Tyr","His","Arg","Asn","Asp","Thr","Stop")
+AA = data.frame(A,AAA)
+data = merge(data,AA, by.x = 'ancestral_aa',by.y='A'); data$ancestral_aa = data$AAA
+data = merge(data,AA, by.x = 'derived_aa',by.y='A'); data$derived_aa = data$AAA.y
+data$FromTo = paste(data$ancestral_aa,data$derived_aa,sep = ">")
+VecOfNames = c("Tier2","chrom","position","FromTo","ancestral_aa","derived_aa")
+data = data[colnames(data) %in% VecOfNames]
+data$FreqOfAASub = 1
 
-FourNonZeroTransitionsMammals = merge(FourNonZeroTransitions,GL, by = 'Species')
-summary(FourNonZeroTransitionsMammals$Ah_GhDivideByTh_Ch)
-summary(FourNonZeroTransitionsMammals$Ch_ThDivideByGh_Ah)
-boxplot(FourNonZeroTransitionsMammals$Ah_GhDivideByTh_Ch,FourNonZeroTransitionsMammals$Ch_ThDivideByGh_Ah, names = c('Ah_GhDivideByTh_Ch','Ch_ThDivideByGh_Ah'), notch = TRUE, outline = FALSE, main = 'mammals', ylab = 'asymmetry')
+####
 
-cor.test(FourNonZeroTransitionsMammals$GenerationLength_d,FourNonZeroTransitionsMammals$Ah_GhDivideByTh_Ch, method = 'spearman') # nothing
-cor.test(FourNonZeroTransitionsMammals$GenerationLength_d,FourNonZeroTransitionsMammals$Ch_ThDivideByGh_Ah, method = 'spearman') # a bit positive
+FromTo = aggregate(data$FreqOfAASub, by = list(data$FromTo), FUN = sum)
+names(FromTo) = c('FromAncestralToDerived','NumberOfEvents')
 
-median(FourNonZeroTransitionsMammals$GenerationLength_d) 
-FourNonZeroTransitionsMammalsShortLived = FourNonZeroTransitionsMammals[FourNonZeroTransitionsMammals$GenerationLength_d < median(FourNonZeroTransitionsMammals$GenerationLength_d),]
-FourNonZeroTransitionsMammalsLongLived = FourNonZeroTransitionsMammals[FourNonZeroTransitionsMammals$GenerationLength_d >= median(FourNonZeroTransitionsMammals$GenerationLength_d),]
+##### ADD DUMMY MATRIX WITH ZEROES 
+AllAa1 = data.frame(unique(data$ancestral_aa)); nrow(AllAa1); names(AllAa1) = c('AA1')
+AllAa2 = data.frame(unique(data$ancestral_aa)); nrow(AllAa2); names(AllAa2) = c('AA2')
+DummyZeroes = merge(AllAa1,AllAa2)
+DummyZeroes$FromAncestralToDerived=paste(DummyZeroes$AA1,DummyZeroes$AA2,sep='>')
+DummyZeroes$NumberOfEvents = 0
+DummyZeroes = DummyZeroes[c(3,4)]
 
-summary(FourNonZeroTransitionsMammalsShortLived$Ah_GhDivideByTh_Ch)
-summary(FourNonZeroTransitionsMammalsShortLived$Ch_ThDivideByGh_Ah)
+##### rbind and aggregate FromTo and DummyZeroes
+FromTo1 = rbind(FromTo,DummyZeroes)
+FromTo2 = aggregate(FromTo1$NumberOfEvents, by = list(FromTo1$FromAncestralToDerived), FUN = sum)
+names(FromTo2)=c('FromAncestralToDerived','NumberOfEvents')
+FromTo2$From = gsub(">.*",'',FromTo2$FromAncestralToDerived)
+FromTo2$To = gsub(".*>",'',FromTo2$FromAncestralToDerived)
+FromTo2 = FromTo2[FromTo2$From != FromTo2$To,]; nrow(FromTo2) # filter out synonymous changes
+# for (i in 1:nrow(FromTo2)) {FromTo2$AaPairId[i] = paste(sort(unlist(strsplit(FromTo2$ExpectedAminoAcidSubstBias[i],'>'))),collapse = '>') }
 
-summary(FourNonZeroTransitionsMammalsLongLived$Ah_GhDivideByTh_Ch)
-summary(FourNonZeroTransitionsMammalsLongLived$Ch_ThDivideByGh_Ah)
+##### DERIVE UNIQUE IDENTIFIER FOR EACH PAIR OF AMINOACIDS BASED ON ALPHABET:
+for (i in 1:nrow(FromTo2)) {FromTo2$AaPairId[i] = paste(sort(unlist(strsplit(FromTo2$FromAncestralToDerived[i],'>'))),collapse = '>') }
 
+##### from 420 to 210 rows and NumberOfEvents1To2
+A = FromTo2[FromTo2$FromAncestralToDerived == FromTo2$AaPairId,]; A= A[c(1,2,5)]; names(A)=c('FromAncestralToDerived1','NumberOfEvents1','AaPairId')
+B = FromTo2[FromTo2$FromAncestralToDerived != FromTo2$AaPairId,]; B= B[c(1,2,5)]; names(B)=c('FromAncestralToDerived2','NumberOfEvents2','AaPairId')
+FromTo3 = merge(A,B)
+FromTo3$NumberOfEvents1To2 = FromTo3$NumberOfEvents1 / FromTo3$NumberOfEvents2 
+
+##### read expectations, add AaPairId and merge
+exp = read.table("../../Body/2Derived/ExpectedAaSubstitutionDirection20AA.txt", header = TRUE, sep = " ")
+for (i in 1:nrow(exp)) {exp$AaPairId[i] = paste(sort(unlist(strsplit(exp$ExpectedAminoAcidSubstBias[i],'>'))),collapse = '>') }
+# exp=exp[c(3,4)]
+FromTo4 = merge(FromTo3,exp, by = 'AaPairId', all.x = TRUE)
+
+##### 
+FromTo4$ExpectedMoreThanOne=NA
+for (i in 1:nrow(FromTo4))
+{ # i = 17
+  if (!is.na(FromTo4$ExpectedAminoAcidSubstBias[i]) & FromTo4$ExpectedAminoAcidSubstBias[i] == FromTo4$FromAncestralToDerived1[i]) {FromTo4$ExpectedMoreThanOne[i] = FromTo4$NumberOfEvents1[i] / FromTo4$NumberOfEvents2[i]}
+  if (!is.na(FromTo4$ExpectedAminoAcidSubstBias[i]) & FromTo4$ExpectedAminoAcidSubstBias[i] == FromTo4$FromAncestralToDerived2[i]) {FromTo4$ExpectedMoreThanOne[i] = FromTo4$NumberOfEvents2[i] / FromTo4$NumberOfEvents1[i]}
+}
+
+FromTo4 = FromTo4[order(FromTo4$ExpectedMoreThanOne),]
+summary(FromTo4$ExpectedMoreThanOne) # 0.5789  4.9191  7.3214     Inf 12.0074     Inf     167
+
+# delete variants from stop or to stop (too rare and probably deleterious => mistakes):
+FromTo5 = FromTo4[!grepl('Stop',FromTo4$AaPairId),] 
+
+summary(FromTo5$ExpectedMoreThanOne)
+
+# derive 'NumberOfExpectedAaSubst' 'NumberOfUnexpectedAaSubst'
+FromTo5$NumberOfExpectedAaSubst = 0
+FromTo5$NumberOfUnexpectedAaSubst = 0
+for (i in 1:nrow(FromTo5[!is.na(FromTo5$ExpectedMoreThanOne),]))
+{ # i = 1
+  if (FromTo5$FromAncestralToDerived1[i] == FromTo5$ExpectedAminoAcidSubstBias[i]) {FromTo5$NumberOfExpectedAaSubst[i] = FromTo5$NumberOfEvents1[i]; FromTo5$NumberOfUnexpectedAaSubst[i] = FromTo5$NumberOfEvents2[i];}
+  if (FromTo5$FromAncestralToDerived2[i] == FromTo5$ExpectedAminoAcidSubstBias[i]) {FromTo5$NumberOfExpectedAaSubst[i] = FromTo5$NumberOfEvents2[i]; FromTo5$NumberOfUnexpectedAaSubst[i] = FromTo5$NumberOfEvents1[i];}
+}
+
+hist(FromTo5$ExpectedMoreThanOne,breaks = 100)
+wilcox.test(FromTo5$ExpectedMoreThanOne, mu = 1) # PAPER !!!! p-value = 7.153e-07
+
+FromTo5$TotalSubst = FromTo5$NumberOfExpectedAaSubst + FromTo5$NumberOfUnexpectedAaSubst
+Short = FromTo5[!is.na(FromTo5$ExpectedMoreThanOne),]
+Short$DummyAhGh = 0
+for (i in 1:nrow(Short))
+{
+  if (Short$NuclSubstLightChainNotation[i] == 'T>C') {Short$DummyAhGh[i] = 1}
+  if (Short$NuclSubstLightChainNotation[i] == 'G>A') {Short$DummyAhGh[i] = 0}
+}
+
+cor.test(Short$ExpectedMoreThanOne,Short$GranthamDistance, method = 'spearman') # a bit negative!!! 
+cor.test(Short$TotalSubst,Short$GranthamDistance, method = 'spearman') # a bit negative
+cor.test(Short$ExpectedMoreThanOne,Short$TotalSubst, method = 'spearman') 
+#summary(lm(Short$ExpectedMoreThanOne ~ Short$TotalSubst*Short$GranthamDistance))
+#summary(lm(Short[Short$NuclSubstLightChainNotation == 'T>C',]$ExpectedMoreThanOne ~ Short[Short$NuclSubstLightChainNotation == 'T>C',]$TotalSubst + Short[Short$NuclSubstLightChainNotation == 'T>C',]$GranthamDistance))
+
+## bias is higher in Ah>Gh than Ch>Th!!! why? Ah>Gh is more asymmetric on average???!!! may be yes!? check human global tree and mammalian average piechart
+wilcox.test(Short[Short$NuclSubstLightChainNotation == 'G>A',]$ExpectedMoreThanOne,Short[Short$NuclSubstLightChainNotation == 'T>C',]$ExpectedMoreThanOne)
+boxplot(Short[Short$NuclSubstLightChainNotation == 'G>A',]$ExpectedMoreThanOne,Short[Short$NuclSubstLightChainNotation == 'T>C',]$ExpectedMoreThanOne, names = c('Ch>Th','Ah>Gh'), ylab = 'expected shift')
+
+#summary(lm(Short$ExpectedMoreThanOne ~ Short$DummyAhGh)) # significant
+
+Short = Short[,-c(2:6)]
+write.table(Short,"../../Body/3Results/Alima04.AsymmetryInCancers.txt")
 dev.off()
+
+### compare humans and vertebrates: !!!!!!!!!!! CAN'T MERGE BECAUSE OF LEU AND SER!!!!!
+ShortHumans = read.table("../../Body/3Results/Alima02.AaAsymmetry.ExpectedVsObservedAaChanges.txt", header = TRUE)
+ShortHumans = ShortHumans[,c(4,6)]; names(ShortHumans)[2] = c('Humans.ExpectedMoreThanOne')
+Short = merge(Short,ShortHumans)
+cor.test(Short$ExpectedMoreThanOne,Short$Humans.ExpectedMoreThanOne, method = 'spearman') # non significant. May be will be significant with mammals
