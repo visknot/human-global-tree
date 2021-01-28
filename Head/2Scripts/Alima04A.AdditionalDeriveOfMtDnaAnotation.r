027800ccaeea96bf39f9538a2921ea4a9d452882
konstantinpopadin <konstantinpopadin@gmail.com>
2021-01-28 23:00:01 +0100

    cancer multiple models

--- Head/2Scripts/Alima04A.AdditionalDeriveOfMtDnaAnotation.r
+++ Head/2Scripts/Alima04A.AdditionalDeriveOfMtDnaAnotation.r
@@ -4,9 +4,12 @@ rm(list=ls(all=TRUE))
 
 library('Biostrings')
 
-mt = read.table("../../Body/1Raw/mtNucAnnotation_MergeRazerV6TurboMaxPro_normMTcode_normND6_withGERP.csv", header = TRUE, sep = ';')
+#mt = read.table("../../Body/1Raw/mtNucAnnotation_MergeRazerV6TurboMaxPro_normMTcode_normND6_withGERP.csv", header = TRUE, sep = ';')
+mt = read.table("../../Body/1Raw/mtNucAnnotation_MergeRazerV6TurboMaxPro_normMTcode_normND6_withGERP_corrected280121.csv", header = TRUE, sep = ';')
+
 table(mt$role)
 ProtCodGenes = c("mRNA_ATP6","mRNA_ATP6&COX3","mRNA_ATP8","mRNA_ATP8&ATP6","mRNA_COX1","mRNA_COX2","mRNA_COX3","mRNA_CYTB","mRNA_ND1","mRNA_ND2","mRNA_ND3","mRNA_ND4","mRNA_ND4L","mRNA_ND4L&ND4","mRNA_ND5","mRNA_ND6")
+ProtCodGenesExceptNd6 = c("mRNA_ATP6","mRNA_ATP6&COX3","mRNA_ATP8","mRNA_ATP8&ATP6","mRNA_COX1","mRNA_COX2","mRNA_COX3","mRNA_CYTB","mRNA_ND1","mRNA_ND2","mRNA_ND3","mRNA_ND4","mRNA_ND4L","mRNA_ND4L&ND4","mRNA_ND5")
 length(ProtCodGenes) # 13 genes + 3 overlaps
 
 ### for ND6 reverse complement codons
@@ -25,7 +28,7 @@ for (i in 1:nrow(mt))
 mt$MutatedCodon = ''
 for (i in 1:nrow(mt))
 {
-  if (mt$role[i] %in% ProtCodGenes)
+  if (mt$role[i] %in% ProtCodGenesExceptNd6)
   {
   if (mt$nuc[i] == 'T' & mt$nnuc[i] == 1)  {mt$MutatedCodon[i] = paste('C',unlist(strsplit(mt$RnaCodon[i],''))[2],unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
   if (mt$nuc[i] == 'G' & mt$nnuc[i] == 1)  {mt$MutatedCodon[i] = paste('A',unlist(strsplit(mt$RnaCodon[i],''))[2],unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
@@ -42,21 +45,72 @@ for (i in 1:nrow(mt))
   if (mt$nuc[i] == 'C' & mt$nnuc[i] == 3)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],unlist(strsplit(mt$RnaCodon[i],''))[2],'T', sep = '')}
   if (mt$nuc[i] == 'A' & mt$nnuc[i] == 3)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],unlist(strsplit(mt$RnaCodon[i],''))[2],'G', sep = '')}
   }
-}
-
-MitCode = getGeneticCode(id_or_name2="2", full.search=FALSE, as.data.frame=FALSE)
-mt$MutatedAa = ''
-for (i in 1:nrow(mt))
-{ # i = 1
-  if (mt$MutatedCodon[i] != '') 
+  if (mt$role[i] == "mRNA_ND6") # the same as above but with complementarity rules
   {
-  codon = DNAString(x=mt$MutatedCodon[i], start=1, nchar=NA); 
-  AaOneLetter = AAString(translate(codon,MitCode)); 
-  mt$MutatedAa[i] = as.character(AMINO_ACID_CODE[strsplit(as.character(AaOneLetter), NULL)[[1]]]);
+  if (mt$nuc[i] == 'T' & mt$nnuc[i] == 1)  {mt$MutatedCodon[i] = paste('G',unlist(strsplit(mt$RnaCodon[i],''))[2],unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
+  if (mt$nuc[i] == 'G' & mt$nnuc[i] == 1)  {mt$MutatedCodon[i] = paste('T',unlist(strsplit(mt$RnaCodon[i],''))[2],unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
+  if (mt$nuc[i] == 'C' & mt$nnuc[i] == 1)  {mt$MutatedCodon[i] = paste('A',unlist(strsplit(mt$RnaCodon[i],''))[2],unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
+  if (mt$nuc[i] == 'A' & mt$nnuc[i] == 1)  {mt$MutatedCodon[i] = paste('C',unlist(strsplit(mt$RnaCodon[i],''))[2],unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}    
+  
+  if (mt$nuc[i] == 'T' & mt$nnuc[i] == 2)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],'G',unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
+  if (mt$nuc[i] == 'G' & mt$nnuc[i] == 2)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],'T',unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
+  if (mt$nuc[i] == 'C' & mt$nnuc[i] == 2)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],'A',unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
+  if (mt$nuc[i] == 'A' & mt$nnuc[i] == 2)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],'C',unlist(strsplit(mt$RnaCodon[i],''))[3], sep = '')}
+  
+  if (mt$nuc[i] == 'T' & mt$nnuc[i] == 3)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],unlist(strsplit(mt$RnaCodon[i],''))[2],'G', sep = '')}
+  if (mt$nuc[i] == 'G' & mt$nnuc[i] == 3)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],unlist(strsplit(mt$RnaCodon[i],''))[2],'T', sep = '')}
+  if (mt$nuc[i] == 'C' & mt$nnuc[i] == 3)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],unlist(strsplit(mt$RnaCodon[i],''))[2],'A', sep = '')}
+  if (mt$nuc[i] == 'A' & mt$nnuc[i] == 3)  {mt$MutatedCodon[i] = paste(unlist(strsplit(mt$RnaCodon[i],''))[1],unlist(strsplit(mt$RnaCodon[i],''))[2],'C', sep = '')}
   }
 }
-mt$MutatedAa[is.na(mt$MutatedAa)] <- 'STOP'
 
+#MitCode = getGeneticCode(id_or_name2="2", full.search=FALSE, as.data.frame=FALSE)
+
+TranslateMitCodonsIntoThreeLetterAa<-function(x)
+{
+  if (x %in% c('TTT','TTC')) {return ("Phe")}
+  if (x %in% c('TTA','TTG')) {return ("Leu")}
+  if (x %in% c('CTT','CTC','CTA','CTG')) {return ("Leu")}
+  if (x %in% c('ATT','ATC')) {return ("Ile")}
+  if (x %in% c('ATA','ATG')) {return ("Met")}
+  if (x %in% c('GTC','GTA','GTG','GTT')) {return ("Val")}
+  
+  if (x %in% c('TCT','TCC','TCA','TCG')) {return ("Ser")}
+  if (x %in% c('CCT','CCC','CCA','CCG')) {return ("Pro")}
+  if (x %in% c('ACT','ACC','ACA','ACG')) {return ("Thr")}
+  if (x %in% c('GCT','GCC','GCA','GCG')) {return ("Ala")}
+  
+  if (x %in% c('TAT','TAC')) {return ("Tyr")}
+  if (x %in% c('TAA','TAG')) {return ("Stop")}
+  if (x %in% c('CAT','CAC')) {return ("His")}
+  if (x %in% c('CAA','CAG')) {return ("Gln")}
+  if (x %in% c('AAT','AAC')) {return ("Asn")}
+  if (x %in% c('AAA','AAG')) {return ("Lys")}
+  if (x %in% c('GAT','GAC')) {return ("Asp")}
+  if (x %in% c('GAA','GAG')) {return ("Glu")}
+  
+  if (x %in% c('TGT','TGC')) {return ("Cys")}
+  if (x %in% c('TGA','TGG')) {return ("Trp")}
+  if (x %in% c('CGT','CGC','CGA','CGG')) {return ("Arg")}
+  if (x %in% c('AGT','AGC')) {return ("Ser")}
+  if (x %in% c('AGA','AGG')) {return ("Stop")}
+  if (x %in% c('GGT','GGC','GGA','GGG')) {return ("Gly")}
+}
+# TranslateMitCodonsIntoThreeLetterAa('TTC')
+# MitCode = getGeneticCode("SGC1")
+mt$MutatedAa = ''
+for (i in 1:nrow(mt))
+{ # i = 7599
+  if (mt$MutatedCodon[i] != '') 
+  {
+  mt$MutatedAa[i] = TranslateMitCodonsIntoThreeLetterAa(mt$MutatedCodon[i])
+  #codon = DNAString(x=mt$MutatedCodon[i], start=1, nchar=NA); 
+  # AaOneLetter = AAString(translate(codon,genetic.code=MitCode)); 
+  # mt$MutatedAa[i] = as.character(AMINO_ACID_CODE[strsplit(as.character(AaOneLetter), NULL)[[1]]]);
+  }
+}
+
+mt$acid = gsub("STOP",'Stop',mt$acid)
 mt$AaSub = ''
 for (i in 1:nrow(mt))
 { # i = 10500
@@ -69,6 +123,7 @@ for (i in 1:nrow(mt))
       }
   }
 }
+table(mt$AaSub)
 
 mt$TimeBeingSingleStranded = 0
 for (i in 1:nrow(mt))
---
+++ Head/2Scripts/Alima04B.AsymmetryInCancers.MultModel.r
@@ -0,0 +1,145 @@
+rm(list=ls(all=TRUE))
+
+##### 1. read expected variants (27) and make them bidirectional (all four transitions): AaPredictions (54)
+
+exp = read.table("../../Body/2Derived/ExpectedAaSubstitutionDirection20AA.txt", header = TRUE, sep = " ")
+for (i in 1:nrow(exp)) {exp$AaPairId[i] = paste(sort(unlist(strsplit(exp$ExpectedAminoAcidSubstBias[i],'>'))),collapse = '>') }
+exp$ExpDummyFromLoserToGainer = 1
+exp$ChToThDummy = gsub('G>A',1,exp$NuclSubstLightChainNotation)
+exp$ChToThDummy = gsub('T>C',0,exp$ChToThDummy)
+exp=exp[,-c(2,5)]
+names(exp)[2] = c('AaSub')
+names(exp)
+
+TO = gsub("(.*)>",'',exp$AaSub); FROM = gsub(">(.*)",'',exp$AaSub); 
+unexp = paste(TO,FROM,sep = '>')
+
+unexp = data.frame(unexp,exp$GranthamDistance,exp$NuclPosInCodon,exp$ChToThDummy)
+unexp$ExpDummyFromLoserToGainer = 0
+names(unexp)=c('AaSub','GranthamDistance','NuclPosInCodon','ChToThDummy','ExpDummyFromLoserToGainer')
+AaPredictions = rbind(exp,unexp)
+AaPredictions[is.na(AaPredictions$GranthamDistance),]$GranthamDistance <-300 # make from stop or to stop equals 300 (max)
+
+AaPredictions$QuadrantOne = 0; AaPredictions$QuadrantTwo = 0; AaPredictions$QuadrantThree = 0;  AaPredictions$QuadrantFour = 0; 
+for (i in 1:nrow(AaPredictions))
+{ # i = 1
+  if (grepl("Gln|His|Tyr|Cys|Trp|Arg",AaPredictions$AaSub[i])) {AaPredictions$QuadrantOne[i] = 1}
+  if (grepl("Phe|Leu|Pro",AaPredictions$AaSub[i])) {AaPredictions$QuadrantTwo[i] = 1}
+  if (grepl("Ile|Met|Val|Thr|Ala",AaPredictions$AaSub[i])) {AaPredictions$QuadrantThree[i] = 1}
+  if (grepl("Asn|Lys|Asp|Glu|Gly",AaPredictions$AaSub[i])) {AaPredictions$QuadrantFour[i] = 1}
+}
+
+##### 2. read extensively mutated file, take only nons and only driven by two most common transitions and make base
+
+MitAnno = read.table("../../Body/2Derived/'mtNucAnnotation_MergeRazerV6TurboMaxPro_normMTcode_normND6_withGERP.AllTransitions.Tbss.txt")
+MitAnno = MitAnno[MitAnno$AaSub!='',] # 7155
+MitAnno = merge(MitAnno,AaPredictions,by="AaSub") # 7146 (catch the difference!!!!!!!!!!! Victor's AGA  in ND6 is translated as stop, but it should be  TCT (Ser)
+#MitAnno = merge(MitAnno,AaPredictions,by="AaSub",all.x=TRUE) # 6504 => 7597-6504 (1093 substitutions were not in MitAnno - which ones)
+#table(MitAnno$AaSub)
+#MitAnnoNa = MitAnno[is.na(MitAnno$ExpDummy),] # zero
+
+
+#mut = mut[mut$SubstType == 'missense_variant',]
+#mut = mut[,c(2,4,5,8,9)]
+#names(mut)=c('position','Ref','Alt','Annotation','SubstType')
+# save only nonsynonymous, overlapped with 27*2 above
+#mut$NucSub = paste(mut$Ref,mut$Alt,sep = '>')
+#table(mut$NucSub)
+#mut = mut[mut$NucSub %in% c('C>T','T>C','G>A','A>G'),]
+# save only transitions
+
+# ReturnEleven <- function(x) 
+# { # temp = 'p.Asp139Asn'
+#  temp = unlist(strsplit(x, split = "\\|"))[11]; 
+#  Aa1 = paste(unlist(strsplit(temp,split=''))[c(3,4,5)], collapse = '')
+#  Length = length(unlist(strsplit(temp,split='')));
+#  Aa2 = paste(unlist(strsplit(temp,split=''))[c(Length-2,Length-1,Length)], collapse = '')
+#  AaSubs = paste(Aa1,Aa2,sep='>')
+#  }
+# mut$AaSub = apply(as.matrix(mut$Annotation),1,ReturnEleven)
+# table(mut$AaSub)
+# MutShort = mut[names(mut) %in% c('position','NucSub','AaSub')]
+# MutShort$mutation = 0 # it means mutation which might happen
+
+names(MitAnno)
+for (i in 1:nrow(MitAnno))
+{
+ if (MitAnno$nuc[i] == 'G')   {MitAnno$NucSub[i] = 'G>A'}
+ if (MitAnno$nuc[i] == 'A')   {MitAnno$NucSub[i] = 'A>G'}
+ if (MitAnno$nuc[i] == 'T')   {MitAnno$NucSub[i] = 'T>C'}
+ if (MitAnno$nuc[i] == 'C')   {MitAnno$NucSub[i] = 'C>T'}
+}
+MitAnno$position = MitAnno$pos
+MitAnno$mutation = 0
+MitAnno = MitAnno[names(MitAnno) %in% c('position','NucSub','AaSub','mutation')]
+
+##### 3. READ CANCER DATA AND REMOVE EVERYTHING EXCEPT 27*2 
+data = read.table("../../Body/1Raw/CancerDataFromCampbell/mtDNA_snv_Oct2016.txt", header = TRUE, sep = '\t')
+for (i in 1:nrow(data)) {data$AaChanges[i] = unlist(strsplit(data$Annot[i],','))[5]}
+data$ancestral_aa = gsub("\\d(.*)",'',data$AaChanges)
+data$derived_aa = gsub("(.*)\\d",'',data$AaChanges)
+data=data[grepl('nsSNP', data$Annot),]
+data$NucSub = paste(data$ref,data$var, sep = '>')
+data$AaSub = paste(data$ancestral_aa,data$derived_aa, sep = '>')
+table(data$AaSub)
+
+# from one letter to three letter code AA:
+A = c("G","A","L","M","F","W","K","Q","E","S","P","V","I","C","Y","H","R","N","D","T","*")
+AAA = c("Gly","Ala","Leu","Met","Phe","Trp","Lys","Gln","Glu","Ser","Pro","Val","Ile","Cys","Tyr","His","Arg","Asn","Asp","Thr","Stop")
+AA = data.frame(A,AAA)
+data = merge(data,AA, by.x = 'ancestral_aa',by.y='A'); data$ancestral_aa = data$AAA
+data = merge(data,AA, by.x = 'derived_aa',by.y='A'); data$derived_aa = data$AAA.y
+data$AaSub = paste(data$ancestral_aa,data$derived_aa,sep = ">")
+VecOfNames = c("position","NucSub","AaSub")
+data = data[colnames(data) %in% VecOfNames]
+names(data) # "position" "NucSub"   "AaSub" 
+data$mutation = 1 # it means real mutation
+nrow(data) # 3805
+data = data[data$AaSub %in% AaPredictions$AaSub,] # delete all substitutions due to transversions and double mutations
+nrow(data) # 3540
+
+### rbind cancer with MitAnno and aggregate
+
+data = rbind(data,MitAnno)
+agg = aggregate(data$mutation, by = list(data$position,data$NucSub,data$AaSub), FUN = sum)
+names(agg) = c('position','NucSub','AaSub','mutation')
+agg = agg[order(agg$position),]
+nrow(agg) # 7160
+length(unique(agg$position)) # 7159 !!!!!!!!!!!!!!!!!!! should be the same as line above!!!!!!!!
+DuplPositions = agg[duplicated(agg$position),]$position; length(DuplPositions)
+DuplPositions # 12766  !!!!!!!!!!!!!!!!!!!!!!!!!!! should be ZERO
+Problems = agg[agg$position %in% DuplPositions,]
+
+agg = agg[!agg$position %in% DuplPositions,]   # ZAPLATKA!!!!!!!!!!!!!
+
+#### merge agg with AaPredictions
+agg = merge(agg,AaPredictions, by = 'AaSub')
+
+#### merge agg with Victor's annotation
+MitAnno = read.table("../../Body/2Derived/'mtNucAnnotation_MergeRazerV6TurboMaxPro_normMTcode_normND6_withGERP.AllTransitions.Tbss.txt")
+nrow(agg)
+agg = merge(agg,MitAnno, by.x = 'position', by.y = 'pos')
+nrow(agg)
+
+table(agg$role)
+# derive DummyNd6!!!!!!!!!!!!!!!
+agg$DummyNd6 = 0; 
+for (i in 1:nrow(agg)) {if (agg$role[i] == 'mRNA_ND6') {agg$DummyNd6[i] = 1}}
+names(agg)
+
+################################
+########### analysis
+################################
+
+summary(glm(mutation ~ ExpDummyFromLoserToGainer + TimeBeingSingleStranded + GranthamDistance + ChToThDummy + DummyNd6, data = agg, family = 'poisson'))
+summary(glm(mutation ~ (ExpDummyFromLoserToGainer + TimeBeingSingleStranded + GranthamDistance + ChToThDummy)*DummyNd6, data = agg, family = 'poisson'))
+
+summary(glm(mutation ~ ExpDummyFromLoserToGainer*DummyNd6 + ChToThDummy + scale(TimeBeingSingleStranded) + scale(GranthamDistance), data = agg, family = 'poisson'))
+summary(glm(mutation ~ (ExpDummyFromLoserToGainer + ChToThDummy)*DummyNd6 + scale(TimeBeingSingleStranded) + scale(GranthamDistance), data = agg, family = 'poisson')) # final
+summary(glm(mutation ~ ExpDummyFromLoserToGainer*DummyNd6 + scale(TimeBeingSingleStranded) + scale(GranthamDistance), data = agg, family = 'poisson'))
+
+### Quadrant 3 is a bit more mutagenic... why? low Grantham? 
+summary(glm(mutation ~ (ExpDummyFromLoserToGainer + ChToThDummy)*DummyNd6 + scale(TimeBeingSingleStranded) + scale(GranthamDistance) + QuadrantOne + QuadrantTwo + QuadrantThree + QuadrantFour, data = agg, family = 'poisson')) # final
+summary(glm(mutation ~ (ExpDummyFromLoserToGainer + ChToThDummy)*DummyNd6 + scale(TimeBeingSingleStranded) + scale(GranthamDistance) + QuadrantThree + QuadrantFour, data = agg, family = 'poisson')) # final
+summary(glm(mutation ~ (ExpDummyFromLoserToGainer + ChToThDummy)*DummyNd6 + scale(TimeBeingSingleStranded) + scale(GranthamDistance) + QuadrantThree, data = agg, family = 'poisson')) # final
+
