e5869939d38be2a004346967cecce3b6d0ddb65a
konstantinpopadin <konstantinpopadin@gmail.com>
2021-05-19 17:37:03 +0200

    Alima10A - little differences

--- Head/2Scripts/Alima10.DogCancer.r
+++ Head/2Scripts/Alima10.DogCancer.r
@@ -1,20 +1,30 @@
 rm(list=ls(all=TRUE)) 
 
 sm4b = read.table("../../Body/1Raw/DogCancer/Strakova2016SupplMat4b.txt", header = TRUE, quote = '')
+table(sm4b$Substitution)
 sm4b$SnpId = paste(sm4b$VariantPosition ,sm4b$Substitution,sep='.')
-length(unique(sm4b$SnpId))
+length(unique(sm4b$SnpId)) # 814
+
 sm6a = read.table("../../Body/1Raw/DogCancer/Strakova2016SupplMat6a.txt", header = TRUE, quote = '', sep = '\t')
 sm6a$SnpId = paste(sm6a$Position ,sm6a$BaseChangeRefToAlt,sep='.')
-length(unique(sm6a$SnpId))
+head(sm6a)
+length(unique(sm6a$SnpId)) # 882
 
-nrow(sm4b) # 928
+nrow(sm4b) # 928 > 814 ????? VAF !!!!!!!
 mut = merge(sm4b,sm6a, by = 'SnpId')
-nrow(mut) # 929 one more!!!!
+nrow(mut) # 929 one more!!!!!
 
+head(mut)
 table(mut$Impact)
-mut = mut[mut$Impact == 'Non-synonymous variant',]
-table(mut$UniProtKBGeneName)
-mut = mut[mut$UniProtKBGeneName != 'ND6',]
 
-table(mut$AminoAcidChange)
+MutNons = mut[mut$Impact == 'Non-synonymous variant',]
+table(MutNons$UniProtKBGeneName)
+MutNons = MutNons[MutNons$UniProtKBGeneName != 'ND6',]
+
+### observed:
+table(MutNons$AminoAcidChange)
+table(MutNons$AminoAcidChange)
+
+### expected: Sasha Voronka, ideal table for RefSeq mtDNA dog!!!
+
 
